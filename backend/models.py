9b0671547d7cb12e335ed3b2a990cf9b3120f7b8
AkashVachagan <akash.vachagan@gmail.com>
2025-09-02 17:46:24 +0530

    Few changes to database colns

--- backend/models.py
+++ backend/models.py
@@ -12,13 +12,12 @@ def create_table(table_name: str):
     class Task(Base):
         __tablename__ = table_name
         __table_args__ = {'extend_existing': True}
+
         id = Column(Integer, primary_key=True, index=True)
         title = Column(String(255), nullable=False)
         description = Column(Text, nullable=True)
-        completed = Column(Boolean, default=False)
         status = Column(sqlEnum(StatusEnum), default=StatusEnum.new)
+        due_date = Column(DateTime(timezone=True), nullable=True)
         created_on = Column(DateTime(timezone=True), server_default=func.now())
         updated_on = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
-        scheduled = Column(DateTime(timezone=True), nullable=True)
-        is_scheduled = Column(Boolean, default=False)
     return Task
